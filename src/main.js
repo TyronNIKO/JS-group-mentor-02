97cce2a7e9c8e1a861657e625f27495a021fd87c
Volodymyr Bilohorodskyi <niko.fm@gmail.com>
2024-06-08 11:36:39 +0300

    Task-4 done

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
+import { nanoid } from 'nanoid';
+
 //TODO-1
 // Напишіть логіку обробнику подій по сабміту
 // При сабміті треба у змінну записувати значення поля інпута
 // Повинна бути перевірка на порожнє поле.
 const formEl = document.querySelector('#task-form');
-const localStorageKey = "task-key";
-
-
-console.log();
+const ulEl = document.querySelector('#task-list');
+const localStorageKey = 'task-key';
 
 formEl.addEventListener('submit', fooSubmit);
-const ulEl = document.querySelector('#task-list');
 
 function fooSubmit(e) {
   e.preventDefault();
@@ -18,25 +17,29 @@ function fooSubmit(e) {
     return;
   }
   console.log(inputValue);
+  const newId = nanoid(); //=> "V1StGXR8_Z5jdHi6B-myT"
 
-  ulEl.insertAdjacentHTML('beforeend', createLiEl(inputValue));
-  addTask(inputValue);
+  ulEl.insertAdjacentHTML('beforeend', createLiEl(inputValue, newId));
+  addTask(inputValue, newId);
   e.target.reset();
 }
 //TODO-2
 // Напишіть логіку яка з сабміта буде брати значення поля інпут
 // Генерувати елемент списку LI  з текстом і кнопкою Х, у майбутньому це буде кнопка видалення таски
 
-function createLiEl(inputValue) {
-  return `<li class ="task-list-item">${inputValue}<button type="button">X</button></li>`
+function createLiEl(inputValue, id) {
+  return `<li id="${id}" class ="task-list-item">${inputValue}<button type="button">X</button></li>`;
 }
 
 //TODO-3
 // Написати функцію, яка при сабміті буде зберігати данні в сховище, в сховище повинні додаватись таски, а не перезаписуватись існуюча
 
-function addTask(inputValue) {
+function addTask(inputValue, id) {
   const arrTask = JSON.parse(localStorage.getItem(localStorageKey)) || [];
-  arrTask.push(inputValue);
+  const obj = { id: id, text: inputValue };
+  arrTask.push(obj);
   localStorage.setItem(localStorageKey, JSON.stringify(arrTask));
-
 }
+
+//TODO-4
+//Відформатуйте код таким чином, щоб данні в сховищі зберігались у вигляді об'єкта { id: value, text: value}, розмітка додавалась з айдішніком на елемент списку li, айдішнік генерувати з допомогою бібліотеки nanoid, її треба встановити
