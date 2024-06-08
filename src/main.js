f36ca37d4fb4fb537a9d2f17e24e7c38596da5dd
Alinch96 <gusak.alina.9e1@gmail.com>
2024-06-08 10:59:01 +0300

    Added createLiEMarkup

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@
 const formEl = document.querySelector('#task-form');
 console.log(formEl);
 formEl.addEventListener('submit', fooSubmit);
+const ulEl = document.querySelector('#task-list');
 
 function fooSubmit(e) {
   e.preventDefault();
@@ -13,5 +14,14 @@ function fooSubmit(e) {
     return;
   }
   console.log(inputValue);
+
+  ulEl.insertAdjacentHTML('beforeend', createLiEl(inputValue));
   e.target.reset();
+}
+//TODO-2
+// Напишіть логіку яка з сабміта буде брати значення поля інпут
+// Генерувати елемент списку LI  з текстом і кнопкою Х, у майбутньому це буде кнопка видалення таски
+
+function createLiEl(inputValue) {
+  return `<li class ="task-list-item">${inputValue}<button type="button">X</button></li>`
 }
