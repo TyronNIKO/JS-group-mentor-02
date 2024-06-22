47b2aa9220813d2afdb546f5e3ce780c265b07f4
Alinch96 <123462468+Alinch96@users.noreply.github.com>
2024-06-22 09:33:45 +0000

    Deploying to gh-pages from @ TyronNIKO/JS-group-mentor-02@6bcef35b072b4c473a2b4e9b77ed7d1427e3825a 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import{w as r,r as a,L as s}from"./assets/constants-89579bec.js";r(a.favouriteList,s);
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{w as t,d as a,r as e,L as r}from"./assets/deleteElement-8ee6a7bd.js";t(e.favouriteList,r);a(e.favouriteList,r);
 //# sourceMappingURL=commonHelpers2.js.map
---
+++ commonHelpers3.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{a}from"./assets/vendor-e575bd12.js";const t="https://64859281a795d24810b7146a.mockapi.io/work/";async function c(s){try{return(await a.get(`${t}/contacts`)).data}catch(o){console.log(o)}}async function n(){console.log(await c())}n();
+//# sourceMappingURL=commonHelpers3.js.map
