2da11083ad86db3f612202d37ffe67d98b25fc5d
Alinch96 <gusak.alina.9e1@gmail.com>
2024-06-22 13:01:12 +0300

    Finished post, delete, reset

--- src/page-3.js
+++ src/page-3.js
@@ -1,5 +1,21 @@
-import { getUserInfo } from './js/api/contacts-api';
+import { getUsersInfo, getUserInfo, deleteUser, createUser,resetUser } from './js/api/contacts-api';
+
+
 async function allContacts() {
-  console.log(await getUserInfo());
+  const usersData = await getUsersInfo();
+  console.log(usersData);
 }
+
+async function contactById(id) {
+  const userData = await getUserInfo(id);
+  console.log(userData);
+}
+
+// contactById('22');
+
+// deleteUser('22');
+createUser({ name: "Vaska", phone: "1232243546" });
+resetUser('21', {name: 'Sharik', phone: "1232243546" })
+
 allContacts();
+
